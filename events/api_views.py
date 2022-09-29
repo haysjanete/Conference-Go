7411693b0ee825970645fe1dcedfe0fdac6b26d1
Janete <martinezjanete8@gmail.com>
2022-09-28 20:33:42 -0500

    Conference Entity Edit

--- events/api_views.py
+++ events/api_views.py
@@ -15,30 +15,31 @@ class ConferenceListEncoder(ModelEncoder):
     properties = ["name"]
 
 
+@require_http_methods(["GET", "POST"])
 def api_list_conferences(request):
-    """
-    Lists the conference names and the link to the conference.
+    if request.method == "GET":
+        conferences = Conference.objects.all()
+        return JsonResponse(
+            {"conferences": conferences},
+            encoder=ConferenceListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            location = Location.objects.get(id=content["location"])
+            content["location"] = location
+        except Location.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
 
-    Returns a dictionary with a single key "conferences" which
-    is a list of conference names and URLS. Each entry in the list
-    is a dictionary that contains the name of the conference and
-    the link to the conference's information.
-
-    {
-        "conferences": [
-            {
-                "name": conference's name,
-                "href": URL to the conference,
-            },
-            ...
-        ]
-    }
-    """
-    conferences = Conference.objects.all()
-    return JsonResponse(
-        {"conferences": conferences},
-        encoder=ConferenceListEncoder,
-    )
+        conference = Conference.objects.create(**content)
+        return JsonResponse(
+            conference,
+            encoder=ConferenceDetailEncoder,
+            safe=False,
+        )
 
 
 class ConferenceDetailEncoder(ModelEncoder):
