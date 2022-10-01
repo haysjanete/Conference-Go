ce870c4d4b3fdb72dca94af1952f91bc007bf825
Janete <martinezjanete8@gmail.com>
2022-10-01 00:24:17 -0500

    final code push

--- events/api_views.py
+++ events/api_views.py
@@ -3,11 +3,27 @@ from common.json import ModelEncoder
 from .models import Conference, Location, State
 from django.views.decorators.http import require_http_methods
 import json
+from .acls import get_photo, get_weather_data
 
 
 class LocationListEncoder(ModelEncoder):
     model = Location
     properties = ["name"]
+
+
+class LocationDetailEncoder(ModelEncoder):
+    model = Location
+    properties = [
+        "name",
+        "city",
+        "room_count",
+        "created",
+        "updated",
+        "picture_url",
+    ]
+
+    def get_extra_data(self, o):
+        return {"state": o.state.abbreviation}
 
 
 class ConferenceListEncoder(ModelEncoder):
@@ -61,70 +77,24 @@ class ConferenceDetailEncoder(ModelEncoder):
 
 
 def api_show_conference(request, pk):
-    """
-    Returns the details for the Conference model specified
-    by the pk parameter.
-
-    This should return a dictionary with the name, starts,
-    ends, description, created, updated, max_presentations,
-    max_attendees, and a dictionary for the location containing
-    its name and href.
-
-    {
-        "name": the conference's name,
-        "starts": the date/time when the conference starts,
-        "ends": the date/time when the conference ends,
-        "description": the description of the conference,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "max_presentations": the maximum number of presentations,
-        "max_attendees": the maximum number of attendees,
-        "location": {
-            "name": the name of the location,
-            "href": the URL for the location,
-        }
-    }
-    """
     conference = Conference.objects.get(id=pk)
+    weather = get_weather_data(
+        conference.location.city,
+        conference.location.state.abbreviation,
+    )
     return JsonResponse(
-        conference,
+        {"conference": conference, "weather": weather},
         safe=False,
         encoder=ConferenceDetailEncoder,
     )
 
 
-class LocationListEncoder(ModelEncoder):
-    model = Location
-    properties = [
-        "name",
-        "href",
-    ]
-
-
 @require_http_methods(["GET", "POST"])
 def api_list_locations(request):
-    """
-    Lists the location names and the link to the location.
-
-    Returns a dictionary with a single key "locations" which
-    is a list of location names and URLS. Each entry in the list
-    is a dictionary that contains the name of the location and
-    the link to the location's information.
-
-    {
-        "locations": [
-            {
-                "name": location's name,
-                "href": URL to the location,
-            },
-            ...
-        ]
-    }
-    """
     if request.method == "GET":
         locations = Location.objects.all()
         return JsonResponse(
-            {"location": locations},
+            {"locations": locations},
             encoder=LocationListEncoder,
         )
     else:
@@ -138,39 +108,18 @@ def api_list_locations(request):
                 status=400,
             )
 
-
-class LocationDetailEncoder(ModelEncoder):
-    model = Location
-    properties = [
-        "name",
-        "city",
-        "room_count",
-        "created",
-        "updated",
-    ]
-
-    def get_extra_data(self, o):
-        return {"state": o.state.abbreviation}
+        photo = get_photo(content["city"], content["state"].abbreviation)
+        content.update(photo)
+        locations = Location.objects.create(**content)
+        return JsonResponse(
+            locations,
+            encoder=LocationListEncoder,
+            safe=False,
+        )
 
 
 @require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_location(request, pk):
-    """
-    Returns the details for the Location model specified
-    by the pk parameter.
-
-    This should return a dictionary with the name, city,
-    room count, created, updated, and state abbreviation.
-
-    {
-        "name": location's name,
-        "city": location's city,
-        "room_count": the number of rooms available,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "state": the two-letter abbreviation for the state,
-    }
-    """
     if request.method == "GET":
         location = Location.objects.get(id=pk)
         return JsonResponse(
--- presentations/api_views.py
+++ presentations/api_views.py
@@ -1,39 +1,43 @@
 from django.http import JsonResponse
 from .models import Presentation
 from common.json import ModelEncoder
+from events.models import Conference
+from django.views.decorators.http import require_http_methods
+import json
 
 
+class PresentationListEncoder(ModelEncoder):
+    model = Presentation
+    properties = ["title"]
+
+    def get_extra_data(self, o):
+        return {"status": o.status.name}
+
+
+@require_http_methods(["GET", "POST"])
 def api_list_presentations(request, conference_id):
-    """
-    Lists the presentation titles and the link to the
-    presentation for the specified conference id.
-
-    Returns a dictionary with a single key "presentations"
-    which is a list of presentation titles and URLS. Each
-    entry in the list is a dictionary that contains the
-    title of the presentation, the name of its status, and
-    the link to the presentation's information.
-
-    {
-        "presentations": [
-            {
-                "title": presentation's title,
-                "status": presentation's status name
-                "href": URL to the presentation,
-            },
-            ...
-        ]
-    }
-    """
-    presentations = [
-        {
-            "title": p.title,
-            "status": p.status.name,
-            "href": p.get_api_url(),
-        }
-        for p in Presentation.objects.filter(conference=conference_id)
-    ]
-    return JsonResponse({"presentations": presentations})
+    if request.method == "GET":
+        presentation = Presentation.objects.all()
+        return JsonResponse(
+            {"presentation": presentation},
+            encoder=PresentationListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            conference = Conference.objects.get(id=conference_id)
+            content["conference"] = conference
+        except Conference.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+        presentation = Presentation.create(**content)
+        return JsonResponse(
+            presentation,
+            encoder=PresentationListEncoder,
+            safe=False,
+        )
 
 
 class PresentationDetailEncoder(ModelEncoder):
@@ -55,27 +59,3 @@ def api_show_presentation(request, pk):
         encoder=PresentationDetailEncoder,
         safe=False,
     )
-    """
-    Returns the details for the Presentation model specified
-    by the pk parameter.
-
-    This should return a dictionary with the presenter's name,
-    their company name, the presenter's email, the title of
-    the presentation, the synopsis of the presentation, when
-    the presentation record was created, its status name, and
-    a dictionary that has the conference name and its URL
-
-    {
-        "presenter_name": the name of the presenter,
-        "company_name": the name of the presenter's company,
-        "presenter_email": the email address of the presenter,
-        "title": the title of the presentation,
-        "synopsis": the synopsis for the presentation,
-        "created": the date/time when the record was created,
-        "status": the name of the status for the presentation,
-        "conference": {
-            "name": the name of the conference,
-            "href": the URL to the conference,
-        }
-    }
-    """
